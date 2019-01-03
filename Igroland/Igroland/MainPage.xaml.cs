c86126cde6bfbc10ab99c8213def23ad243212b5
YURII <yurii.kalinichenko9@gmail.com>
2019-01-03 21:22:46 +0200

    Добавьте файлы проекта.

---
+++ Igroland/Igroland/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Igroland
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            WebView webView = new WebView() { Source="http://igroland.com.ua"};
+            this.Content = webView;
+        }
+    }
+}
